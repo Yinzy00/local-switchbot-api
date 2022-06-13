09c2ed4188c713e3da67f9c5c8813cfdca22d17a
Yinzy00 <yari.marien@telenet.be>
2022-06-13 22:37:24 +0200

    Added db init to server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
-const express = require('express'),
-    app = express(),
-    routes = require('./routes'),
-    log = require('./log');
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const log = require('./log');
+const { InitDatabase } = require('./database');
 
-
+//Setup express server
 app.use(express.json());
 app.use(routes);
 
+//Create tables if don't exist
+InitDatabase();
 
+//Start server
 app.listen(process.env.SERVER_PORT, () => {
     log(`Server is running on port ${process.env.SERVER_PORT}`);
 });
